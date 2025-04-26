464ab3e242f4241c363d2020f9ae360de086578d
Kalash bharti <kalashbharti2002@gmail.com>
2025-04-27 02:23:36 +0530

    home page added

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { NgClass, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [CardComponent, NgFor,NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  cards = [
+    {img:"assets/images/running.png",  title: "Working Daily", text: "Consistency builds strength, endurance, and unstoppable discipline." },
+    {img:"assets/images/heart.png", title: "Heart Health", text: "A strong heart leads to a longer, healthier life." },
+    {img:"assets/images/heart.png", title: "Make Your Family Healthy", text: "Inspire loved ones; create a legacy of health together." },
+    {img:"assets/images/heart.png", title: "Achieve Fitness Goal", text: "Turn dreams into milestones through focused daily effort." }
+  ]
 }
